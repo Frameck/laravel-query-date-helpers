ad38cd7bbfaad5c0eb2ee5ab1d72d2aea7306881
Frameck <fra9879@gmail.com>
2023-04-15 19:44:45 +0200

    optimized macro registering process

--- src/LaravelQueryDateHelpers.php
+++ src/LaravelQueryDateHelpers.php
@@ -18,155 +18,157 @@ class LaravelQueryDateHelpers
         ];
 
         foreach ($builders as $builder) {
-            app($builder)::macro('yesterday', fn (?string $column = null) => (
+            $builder = app($builder);
+
+            $builder::macro('yesterday', fn (?string $column = null) => (
                 DateService::yesterday($this, $column)
             ));
 
-            app($builder)::macro('today', fn (?string $column = null) => (
+            $builder::macro('today', fn (?string $column = null) => (
                 DateService::today($this, $column)
             ));
 
-            app($builder)::macro('tomorrow', fn (?string $column = null) => (
+            $builder::macro('tomorrow', fn (?string $column = null) => (
                 DateService::tomorrow($this, $column)
             ));
 
-            app($builder)::macro('betweenDates', fn (?Carbon $dateStart = null, ?Carbon $dateEnd = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('betweenDates', fn (?Carbon $dateStart = null, ?Carbon $dateEnd = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::betweenDates($this, $dateStart, $dateEnd, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('weekToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('weekToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::weekToDate($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('monthToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('monthToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::monthToDate($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('quarterToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('quarterToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::quarterToDate($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('yearToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('yearToDate', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::yearToDate($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastMinute', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastMinute', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastMinute($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastHour', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastHour', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastHour($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastWeek', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastWeek', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastWeek($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastMonth', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastMonth', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastMonth($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastQuarter', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastQuarter', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastQuarter($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastYear', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastYear', fn (?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastYear($this, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastMinutes', fn (int $numberOfMinutes = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastMinutes', fn (int $numberOfMinutes = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastMinutes($this, $numberOfMinutes, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastHours', fn (int $numberOfHours = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastHours', fn (int $numberOfHours = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastHours($this, $numberOfHours, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastDays', fn (int $numberOfDays = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastDays', fn (int $numberOfDays = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastDays($this, $numberOfDays, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastWeeks', fn (int $numberOfWeeks = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastWeeks', fn (int $numberOfWeeks = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastWeeks($this, $numberOfWeeks, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastMonths', fn (int $numberOfMonths = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastMonths', fn (int $numberOfMonths = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastMonths($this, $numberOfMonths, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastQuarters', fn (int $numberOfQuarters = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastQuarters', fn (int $numberOfQuarters = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastQuarters($this, $numberOfQuarters, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('lastYears', fn (int $numberOfYears = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('lastYears', fn (int $numberOfYears = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::lastYears($this, $numberOfYears, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('thisWeek', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('thisWeek', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::thisWeek($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('thisMonth', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('thisMonth', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::thisMonth($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('thisQuarter', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('thisQuarter', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::thisQuarter($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('thisYear', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('thisYear', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::thisYear($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextMinute', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextMinute', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextMinute($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextHour', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextHour', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextHour($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextWeek', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextWeek', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextWeek($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextMonth', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextMonth', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextMonth($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextQuarter', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextQuarter', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextQuarter($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextYear', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextYear', fn (?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextYear($this, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextMinutes', fn (int $numberOfMinutes = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextMinutes', fn (int $numberOfMinutes = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextMinutes($this, $numberOfMinutes, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextHours', fn (int $numberOfHours = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextHours', fn (int $numberOfHours = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextHours($this, $numberOfHours, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextDays', fn (int $numberOfDays = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextDays', fn (int $numberOfDays = 7, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextDays($this, $numberOfDays, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextWeeks', fn (int $numberOfWeeks = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextWeeks', fn (int $numberOfWeeks = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextWeeks($this, $numberOfWeeks, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextMonths', fn (int $numberOfMonths = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextMonths', fn (int $numberOfMonths = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextMonths($this, $numberOfMonths, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextQuarters', fn (int $numberOfQuarters = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextQuarters', fn (int $numberOfQuarters = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextQuarters($this, $numberOfQuarters, $date, $column, $dateRangeType)
             ));
 
-            app($builder)::macro('nextYears', fn (int $numberOfYears = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
+            $builder::macro('nextYears', fn (int $numberOfYears = 2, ?Carbon $date = null, ?string $column = null, ?DateRangeType $dateRangeType = null) => (
                 DateService::nextYears($this, $numberOfYears, $date, $column, $dateRangeType)
             ));
         }
